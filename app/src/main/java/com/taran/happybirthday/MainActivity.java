bf7df261ece19aef0bc963ecdc2728f67d27bbeb
Taranjot220 <s.taranjot58@gmail.com>
2017-08-02 15:30:27 +0300

    Add files via upload

--- app/src/main/java/com/taran/happybirthday/MainActivity.java
+++ app/src/main/java/com/taran/happybirthday/MainActivity.java
@@ -3,7 +3,7 @@ package com.taran.happybirthday;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {                // Main Activity Definition
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
